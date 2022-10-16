33cfda9c8c29630d27003c1b5b629da7d6e415ab
Ali Faizan <alifaizan1983@gmail.com>
2022-10-16 21:11:23 +0530

    added file

--- tours/src/Anime.jsx
+++ tours/src/Anime.jsx
@@ -3,21 +3,36 @@ import axios from "axios";
 // axios.get(baseURL).then((response)=> console.log(response.data));
 function Anime() {
     function dataFetch() {
-        const baseURL = "https://x-math.herokuapp.com/api/random";
+        const baseURL = "https://strangerthings-quotes.vercel.app/api/quotes/5";
         axios.get(baseURL).then((response) => {
             setAnime(response.data);
             console.log(response.data);
         });
     }
     const [anime, setAnime] = useState([]);
+
+    console.log({anime})
+
     useEffect(() => {
         dataFetch();
     }, []);
+
     return (
         <div>
-            <h2>{anime.expression}</h2>
-            <button onClick={dataFetch}>Get Quote</button>
+        {anime?.map((animeItem, index)=>{
+            <div key={index}> 
+                <h2>{animeItem.author}</h2>
+                <h3>{anime.first}</h3>
+                <button onClick={dataFetch}>Get Quote</button>
+            </div>
+        })}
         </div>
     );
 }
 export default Anime;
+
+// // [{
+//     id: 1,
+//     strin: 'vd'
+// }]
+
